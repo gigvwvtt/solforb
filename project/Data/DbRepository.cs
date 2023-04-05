52f68a51a047a5d667223436a9fe7ec470e08ec1
Alexander <47731810+gigvwvtt@users.noreply.github.com>
2023-04-05 23:41:06 +0500

    added CRUD

---
+++ project/Data/DbRepository.cs
@@ -0,0 +1,37 @@
+﻿using project.Models;
+
+namespace project.Data;
+
+public class DbRepository : IDbRepository
+{
+    private readonly AppDbContext _context;
+
+    public DbRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public bool Add(Order order)
+    {
+        _context.Add(order);
+        return Save();
+    }
+
+    public bool Update(Order order)
+    {
+        _context.Update(order);
+        return Save();
+    }
+
+    public bool Delete(Order order)
+    {
+        _context.Remove(order);
+        return Save();
+    }
+
+    public bool Save()
+    {
+        var saved = _context.SaveChanges();
+        return saved > 0;
+    }
+}
---
+++ project/Data/IDbRepository.cs
@@ -0,0 +1,11 @@
+﻿using project.Models;
+
+namespace project.Data;
+
+public interface IDbRepository
+{
+    bool Add(Order order);
+    bool Update(Order order);
+    bool Delete(Order order);
+    bool Save();
+}

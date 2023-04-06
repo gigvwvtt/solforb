89dd0a40fe54a0196ff5ab5e1756ac34da0fa6b8
Alexander <47731810+gigvwvtt@users.noreply.github.com>
2023-04-06 16:19:17 +0500

    added order controller and its view

---
+++ project/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using project.Data;
+using project.Models;
+
+namespace project.Controllers;
+
+public class OrderController : Controller
+{
+    private IDbRepository<Order> _orderRepository;
+
+    public OrderController(IDbRepository<Order> orderRepository)
+    {
+        _orderRepository = orderRepository;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var orders = await _orderRepository.GetAll();
+        return View(orders);
+    }
+
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Order order)
+    {
+        return View("Index");
+    }
+}
